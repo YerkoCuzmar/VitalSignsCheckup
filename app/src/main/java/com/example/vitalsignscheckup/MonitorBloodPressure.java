0454d660bf1eaaa0546dcb93dfe8206882a2fd90
Lucas <lucas.diaz@sansano.usm.cl>
2020-07-15 21:03:25 -0400

    bloodpressure: liz taylor

--- app/src/main/java/com/example/vitalsignscheckup/MonitorBloodPressure.java
+++ app/src/main/java/com/example/vitalsignscheckup/MonitorBloodPressure.java
@@ -11,6 +11,8 @@ import java.util.Objects;
 
 public class MonitorBloodPressure extends AppCompatActivity {
 
+    int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +28,38 @@ public class MonitorBloodPressure extends AppCompatActivity {
         actionBarTitle.setText(R.string.MonitorBloodPressureTitle);
 
         //set valor de 1
-        TextView bp_medicion_mmhg = (TextView)findViewById(R.id.bp_medicion_mmhg);
-        bp_medicion_mmhg.setText("meditionuno");
+        //TextView bp_medicion_mmhg = (TextView)findViewById(R.id.bp_medicion_mmhg);
+        //bp_medicion_mmhg.setText("meditionuno");
         //set valor de 2
-        TextView bp_medicion_mmhg2 = (TextView)findViewById(R.id.bp_medicion_mmhg2);
-        bp_medicion_mmhg2.setText("meditiondos");
+        //TextView bp_medicion_mmhg2 = (TextView)findViewById(R.id.bp_medicion_mmhg2);
+        //bp_medicion_mmhg2.setText("meditiondos");
         //set alert type
         TextView estado = (TextView)findViewById(R.id.estado);
         estado.setText("presión alta");
+
+        final TextView textView = (TextView)findViewById(R.id.bp_medicion_mmhg);
+        final TextView textView2 = (TextView)findViewById(R.id.bp_medicion_mmhg2);
+        Thread t = new Thread(){
+            @Override
+            public void run(){
+                while(!isInterrupted()){
+                    try {
+                        Thread.sleep(1000);  //1000ms = 1 sec
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                count++;
+                                textView.setText(String.valueOf(count));
+                                textView2.setText(String.valueOf(count+80));
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
 
     }
 
