251b074750504e1138a5e11c8a3b0b19ee831fab
Alexander <aruzrojas10@gmail.com>
2020-07-21 02:20:17 -0400

    detectar peaks data

---
+++ app/src/main/java/com/example/vitalsignscheckup/SignalDetector.java
@@ -0,0 +1,83 @@
+package com.example.vitalsignscheckup;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.DoubleSummaryStatistics;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LongSummaryStatistics;
+
+public class SignalDetector {
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public HashMap<String, List> analyzeDataForSignals(List<Double> data, int lag, Double threshold, Double influence) {
+
+        // init stats instance
+        DoubleSummary stats = new DoubleSummary();
+
+        // the results (peaks, 1 or -1) of our algorithm
+        List<Integer> signals = new ArrayList<Integer>(Collections.nCopies(data.size(), 0));
+
+        // filter out the signals (peaks) from our original list (using influence arg)
+        List<Double> filteredData = new ArrayList<Double>(data);
+
+        // the current average of the rolling window
+        List<Double> avgFilter = new ArrayList<Double>(Collections.nCopies(data.size(), 0.0d));
+
+        // the current standard deviation of the rolling window
+        List<Double> stdFilter = new ArrayList<Double>(Collections.nCopies(data.size(), 0.0d));
+
+        // init avgFilter and stdFilter
+        for (int i = 0; i < lag; i++) {
+            stats.accept(data.get(i));
+        }
+        avgFilter.set(lag - 1, stats.getAverage());
+        stdFilter.set(lag - 1, (stats.getStandardDeviation())); // getStandardDeviation() uses sample variance
+
+
+        // loop input starting at end of rolling window
+        for (int i = lag; i < data.size(); i++) {
+
+            // if the distance between the current value and average is enough standard deviations (threshold) away
+            if (Math.abs((data.get(i) - avgFilter.get(i - 1))) > threshold * stdFilter.get(i - 1)) {
+
+                // this is a signal (i.e. peak), determine if it is a positive or negative signal
+                if (data.get(i) > avgFilter.get(i - 1)) {
+                    signals.set(i, 1);
+                } else {
+                    signals.set(i, -1);
+                }
+
+                // filter this signal out using influence
+                filteredData.set(i, (influence * data.get(i)) + ((1 - influence) * filteredData.get(i - 1)));
+            } else {
+                // ensure this signal remains a zero
+                signals.set(i, 0);
+                // ensure this value is not filtered
+                filteredData.set(i, data.get(i));
+            }
+            DoubleSummary stats2 = new DoubleSummary();
+            // update rolling average and deviation
+            for (int j = i - lag; j < i; j++) {
+                stats2.accept(filteredData.get(j));
+            }
+            avgFilter.set(i, stats2.getAverage());
+            stdFilter.set(i, (stats2.getStandardDeviation()));
+            //stats.clear();
+        }
+
+        HashMap<String, List> returnMap = new HashMap<String, List>();
+        returnMap.put("signals", signals);
+        returnMap.put("filteredData", filteredData);
+        returnMap.put("avgFilter", avgFilter);
+        returnMap.put("stdFilter", stdFilter);
+
+        return returnMap;
+
+    } // end
+}
+
