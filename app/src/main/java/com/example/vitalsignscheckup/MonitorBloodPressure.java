35c85b3200066fe23bec19feb53ddb3cda9fcf57
Lucas <lucas.diaz@sansano.usm.cl>
2020-07-20 21:10:39 -0400

    bloodpressure + historial + buttonhistorial

--- app/src/main/java/com/example/vitalsignscheckup/MonitorBloodPressure.java
+++ app/src/main/java/com/example/vitalsignscheckup/MonitorBloodPressure.java
@@ -11,6 +11,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Objects;
@@ -18,6 +21,11 @@ import java.util.Objects;
 public class MonitorBloodPressure extends AppCompatActivity {
 
     int count = 0;
+    int count2 = 80;
+
+    DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    Date date = new Date();
+    String dateformatted = dateFormat.format(date);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +42,12 @@ public class MonitorBloodPressure extends AppCompatActivity {
             }
         });
 
-        //set valor de 1
-        //TextView bp_medicion_mmhg = (TextView)findViewById(R.id.bp_medicion_mmhg);
-        //bp_medicion_mmhg.setText("meditionuno");
-        //set valor de 2
-        //TextView bp_medicion_mmhg2 = (TextView)findViewById(R.id.bp_medicion_mmhg2);
-        //bp_medicion_mmhg2.setText("meditiondos");
-        //set alert type
+
         TextView estado = (TextView)findViewById(R.id.estado);
         estado.setText("presión alta");
+
+        final TextView hist1 = (TextView)findViewById(R.id.hist1);
+        final TextView hist2 = (TextView)findViewById(R.id.hist2);
 
         final TextView textView = (TextView)findViewById(R.id.bp_medicion_mmhg);
         final TextView textView2 = (TextView)findViewById(R.id.bp_medicion_mmhg2);
@@ -56,7 +61,14 @@ public class MonitorBloodPressure extends AppCompatActivity {
                             @Override
                             public void run() {
                                 count++;
+                                count2++;
                                 textView.setText(String.valueOf(count));
+                                textView2.setText(String.valueOf(count2));
+
+                                date = new Date();
+                                dateformatted = dateFormat.format(date);
+                                hist1.setText(dateformatted + "                     " + count2 + " mmHg");
+                                hist2.setText(dateformatted + "                     " + count + " mmHg");
                                 textView2.setText(String.valueOf(count+80));
                                 try {
                                     OutputStreamWriter output = new OutputStreamWriter(openFileOutput("blood_pressure_history.txt", Activity.MODE_APPEND));
