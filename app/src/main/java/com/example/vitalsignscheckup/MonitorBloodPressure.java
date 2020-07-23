54bd1f2ae76a0e73b7fee5c64d1ae5d101be422e
GabrielPR-cl <gabriel.pezoa@sansano.usm.cl>
2020-07-22 20:50:52 -0400

    activity_device en espanish

--- app/src/main/java/com/example/vitalsignscheckup/MonitorBloodPressure.java
+++ app/src/main/java/com/example/vitalsignscheckup/MonitorBloodPressure.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
