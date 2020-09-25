667ad5d7914f77a499a5a178307085e591261321
Alexander <aruzrojas10@gmail.com>
2020-09-25 13:38:29 -0300

    Register con Vista

--- app/src/main/java/com/example/vitalsignscheckup/ActivityRegister.java
+++ app/src/main/java/com/example/vitalsignscheckup/ActivityRegister.java
@@ -1,47 +1,119 @@
 package com.example.vitalsignscheckup;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class ActivityRegister extends AppCompatActivity {
 
-    // layout_register
-    EditText etName, etMobile, etEmail, etPass;
-    Button btnRegister;
-    TextView tvIngresar;
-    CheckBox cbPaciente;
+    //private EditText mEditTextName;
+    //private EditText mEditTextEmail;
+    //private EditText mEditTextPassword;
+    //private Button mButtonRegister;
+
+    private EditText etName, etMobile, etEmail, etPass;
+    private Button btnRegister;
+    private TextView tvIngresar;
+    private CheckBox cbPaciente;
+
+    //variables datos a registrar
+
+    //private String name = "";
+    //private String email = "";
+    //private String password = "";
+
+    private String name = "";
+    private String mobile = "";
+    private String email = "";
+    private String pass = "";
+    private boolean isPaciente = false;
+
+
+    //firebase
+
+    FirebaseAuth mAuth;
+
+    //BD
+
+    DatabaseReference mDataBase;
+
+    //aux
+    //FirebaseDatabase fb;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        instanceRegisterElements();
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
 
-                Intent mainActivityIntent = new Intent(v.getContext(), MainActivity2.class);
+        mAuth = FirebaseAuth.getInstance();
+        mDataBase = FirebaseDatabase.getInstance().getReference();  //nodo principal de la base de datos
+        //fb = FirebaseDatabase.getInstance();
+        //mDataBase = fb.getReference();
+        //vitalsignscheckup-c8844 se llama
 
-                //datos para ingresar a la BD
-                String name = etName.getText().toString();
-                String mobile = etMobile.getText().toString();
-                String email = etEmail.getText().toString();
-                String pass = etPass.getText().toString();
+        //mEditTextName = (EditText) findViewById(R.id.editTextName);
+        //mEditTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        //mEditTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        //mButtonRegister = (Button) findViewById(R.id.btnRegister);
+
+        etName = (EditText) findViewById(R.id.editTextName);
+        etMobile = (EditText) findViewById(R.id.editTextMobile);
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
+        etPass = (EditText) findViewById(R.id.editTextPassword);
+        btnRegister = (Button) findViewById(R.id.cirLoginButton);
+        tvIngresar = (TextView) findViewById(R.id.tvIngresar);
+        cbPaciente = (CheckBox) findViewById(R.id.checkBox);
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                //name = mEditTextName.getText().toString();
+                //email = mEditTextEmail.getText().toString();
+                //password = mEditTextPassword.getText().toString();
+
+                name = etName.getText().toString();
+                mobile = etMobile.getText().toString();
+                email = etEmail.getText().toString();
+                pass = etPass.getText().toString();
                 boolean isPaciente = cbPaciente.isChecked(); //true si es paciente
 
-                startActivity(mainActivityIntent);
+                if (!name.isEmpty() && !email.isEmpty() && !pass.isEmpty() && !mobile.isEmpty()){
+
+                    if(pass.length() >= 6) {
+
+                        registerUser();
+                    }
+                    else{
+                        Toast.makeText(ActivityRegister.this, "El password debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(ActivityRegister.this, "Debes completar todos los datos", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -56,15 +128,60 @@ public class ActivityRegister extends AppCompatActivity {
 
     }
 
-    private void instanceRegisterElements () {
 
-        etName = (EditText) findViewById(R.id.editTextName);
-        etMobile = (EditText) findViewById(R.id.editTextMobile);
-        etEmail = (EditText) findViewById(R.id.editTextEmail);
-        etPass = (EditText) findViewById(R.id.editTextPassword);
-        btnRegister = (Button) findViewById(R.id.cirLoginButton);
-        tvIngresar = (TextView) findViewById(R.id.tvIngresar);
-        cbPaciente = (CheckBox) findViewById(R.id.checkBox);
+    private void registerUser(){
+        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) { //tarea es crear usuario
+                if (task.isSuccessful()){ //almacenar datos en la base de datos
+
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("name", name);
+                    map.put("email", email);
+                    map.put("password", pass);
+                    map.put("mobile", mobile);
+                    map.put("paciente", isPaciente);
+
+                    String id = mAuth.getCurrentUser().getUid(); //obtener id del usuario nuevo
+
+                    mDataBase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if(task2.isSuccessful()){ //tarea ahora es crear datos en la bd
+
+                                startActivity(new Intent(ActivityRegister.this, MainActivity2.class));
+                                finish();
+                            }
+                            else{
+                                System.out.println("salleee");
+                                Toast.makeText(ActivityRegister.this, "No se pudieron crear los datos", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+                }
+                else{
+                    Toast.makeText(ActivityRegister.this, "No se pudo registrar este usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    /*@Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }/
+
+    /*@Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null){
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }*/
+
 
 }
--- app/src/main/java/com/example/vitalsignscheckup/LoginActivity.java
+++ app/src/main/java/com/example/vitalsignscheckup/LoginActivity.java
@@ -2,100 +2,186 @@ package com.example.vitalsignscheckup;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    private static final int RC_SIGN_IN = 123;
-    private static final String PROVEEDOR_DESCONOCIDO = "Proveedor desconocido";
+    //private EditText mEditTextName;
+    //private EditText mEditTextEmail;
+    //private EditText mEditTextPassword;
+    //private Button mButtonRegister;
 
-    TextView tvname;
-    TextView tvemail;
+    private EditText etName, etMobile, etEmail, etPass;
+    private Button btnRegister;
+    private TextView tvIngresar;
+    private CheckBox cbPaciente;
 
-    TextInputLayout tiName, tiMobile, tiEmail, tiPass;
-    EditText etName, etMobile, etEmail, etPass;
+    //variables datos a registrar
+
+    //private String name = "";
+    //private String email = "";
+    //private String password = "";
+
+    private String name = "";
+    private String mobile = "";
+    private String email = "";
+    private String pass = "";
+    private boolean isPaciente = false;
+
+
+    //firebase
+
+    FirebaseAuth mAuth;
+
+    //BD
+
+    DatabaseReference mDataBase;
+
+    //aux
+    //FirebaseDatabase fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
 
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+        mAuth = FirebaseAuth.getInstance();
+        mDataBase = FirebaseDatabase.getInstance().getReference();  //nodo principal de la base de datos
+        //fb = FirebaseDatabase.getInstance();
+        //mDataBase = fb.getReference();
+        //vitalsignscheckup-c8844 se llama
+
+        //mEditTextName = (EditText) findViewById(R.id.editTextName);
+        //mEditTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        //mEditTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        //mButtonRegister = (Button) findViewById(R.id.btnRegister);
+
+        etName = (EditText) findViewById(R.id.editTextName);
+        etMobile = (EditText) findViewById(R.id.editTextMobile);
+        etEmail = (EditText) findViewById(R.id.editTextEmail);
+        etPass = (EditText) findViewById(R.id.editTextPassword);
+        btnRegister = (Button) findViewById(R.id.cirLoginButton);
+        tvIngresar = (TextView) findViewById(R.id.tvIngresar);
+        cbPaciente = (CheckBox) findViewById(R.id.checkBox);
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                //se ejecuta cada vez que se inicia o cierra sesion
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+            public void onClick(View view){
 
-                if (user != null){
-                    // hay inicio de sesion exitoso
-                    onSetDataUser(user.getDisplayName(), user.getEmail());
+                //name = mEditTextName.getText().toString();
+                //email = mEditTextEmail.getText().toString();
+                //password = mEditTextPassword.getText().toString();
+
+                name = etName.getText().toString();
+                mobile = etMobile.getText().toString();
+                email = etEmail.getText().toString();
+                pass = etPass.getText().toString();
+                boolean isPaciente = cbPaciente.isChecked(); //true si es paciente
+
+                if (!name.isEmpty() && !email.isEmpty() && !pass.isEmpty() && !mobile.isEmpty()){
+
+                    if(pass.length() >= 6) {
+
+                        registerUser();
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "El password debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else{
-                    //no hay sesion activa o se cerro la sesion
-                    startActivityForResult(AuthUI.getInstance().
-                            createSignInIntentBuilder().
-                            setIsSmartLockEnabled(false)   //false: recuerda contraseñas y cuentas logeadas
-                            /*.setTosAndPrivacyPolicyUrls()*/
-                            .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
-                            .build(), RC_SIGN_IN );
+                    Toast.makeText(LoginActivity.this, "Debes completar todos los datos", Toast.LENGTH_SHORT).show();
                 }
-
             }
-        };
+        });
+
+        tvIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent loginActivityIntent = new Intent(v.getContext(), ActivityLogin.class);
+                startActivity(loginActivityIntent);
+            }
+        });
+
     }
 
-    private void onSetDataUser(String userDisplayName, String email){
-        tvname = (TextView)findViewById(R.id.tvUserView);
-        tvname.setText(userDisplayName);
-        tvemail = (TextView)findViewById(R.id.tvEmail);
-        tvemail.setText(email);
 
-        tiName = (TextInputLayout) findViewById(R.id.textInputName);
+    private void registerUser(){
+        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) { //tarea es crear usuario
+                if (task.isSuccessful()){ //almacenar datos en la base de datos
 
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("name", name);
+                    map.put("email", email);
+                    map.put("password", pass);
+                    map.put("mobile", mobile);
+                    map.put("paciente", isPaciente);
+
+                    String id = mAuth.getCurrentUser().getUid(); //obtener id del usuario nuevo
+
+                    mDataBase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if(task2.isSuccessful()){ //tarea ahora es crear datos en la bd
+
+                                startActivity(new Intent(LoginActivity.this, MainActivity2.class));
+                                finish();
+                            }
+                            else{
+                                System.out.println("salleee");
+                                Toast.makeText(LoginActivity.this, "No se pudieron crear los datos", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "No se pudo registrar este usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN){
-            if (resultCode == RESULT_OK){ //aqui ya esta iniciada la sesion y se cambia de actividad o se manda msje de bienvenida
-                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show();
-            }
-            else{
-                Toast.makeText(this, "Error, intenta de nuevo", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @Override
+    /*@Override
     protected void onResume() {
         super.onResume();
         mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-    }
+    }/
 
-    @Override
+    /*@Override
     protected void onPause() {
         super.onPause();
         if (mAuthStateListener != null){
             mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
         }
-    }
+    }*/
+
+
 }
