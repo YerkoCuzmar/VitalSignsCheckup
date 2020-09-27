9785499ac00ea2c731b22ae98ee68f4c8b43039e
YerkoCuzmar <yerko.cuzmar.14@sansano.usm.cl>
2020-09-27 18:53:35 -0300

    Servicio para StressLevel

--- app/src/main/java/com/example/vitalsignscheckup/MonitorStressLevel.java
+++ app/src/main/java/com/example/vitalsignscheckup/MonitorStressLevel.java
@@ -1,16 +1,34 @@
 package com.example.vitalsignscheckup;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-import java.util.Objects;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MonitorStressLevel extends AppCompatActivity {
+
+    private static final String TAG = "MonitorStressLevel";
+
+    private ServiceStressLevel mService;                 //servicio
+    private MonitorStressLevelViewModel mViewModel;      //viewModel
+    private TextView stressText;                       //medida de nivel de estres
+
+
+    boolean COLLECT_DATA = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +37,62 @@ public class MonitorStressLevel extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.stressToolbar);
         setSupportActionBar(toolbar);
 
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
+        //TODO: definir stressText
 
+        mViewModel = ViewModelProviders.of(this).get(MonitorStressLevelViewModel.class);
+
+        mViewModel.getBinder().observe(this, new Observer<ServiceStressLevel.MyBinder>(){
+
+            @Override
+            public void onChanged(ServiceStressLevel.MyBinder myBinder){
+                if(myBinder != null){
+                    //Log.d(TAG, "onChanged: connected to service"); no se porque tira error
+                    mService = myBinder.getService();
+                    mService.unPausedPretendLongRunningTask();
+                    mViewModel.setIsPpmUpdating(true);
+                }
+                else {
+                    //Log.d(TAG, "onChanged: unbound from service"); no se porque tira error
+                    mService = null;
+                }
+            }
+        });
+
+        mViewModel.getIsPpmUpdating().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable final Boolean isUpdating) {
+                final Handler handler = new Handler(getMainLooper());
+                final Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        if(isUpdating){
+                            if(mViewModel.getBinder().getValue() != null){
+                                mViewModel.setIsPpmUpdating(false);
+                            }
+                            String progress = String.valueOf(mService.getSL());
+                            stressText.setText(progress);
+                            // TODO: AGREGAR AL HISTORIAL
+                            handler.postDelayed(this, 100);
+                        }
+                        else {
+                            handler.removeCallbacks(this);
+                        }
+                    }
+                };
+
+                if (isUpdating){
+                    handler.postDelayed(runnable, 100);
+                }
+            }
+        });
     }
 
     @Override
@@ -35,4 +107,37 @@ public class MonitorStressLevel extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mViewModel.getBinder() != null){
+            unbindService(mViewModel.getServiceConnection());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: ");
+        super.onResume();
+        startService();
+    }
+
+    private void startService(){
+        Intent serviceIntent = new Intent(this, ServiceStressLevel.class);
+        startService(serviceIntent);
+        bindService();
+    }
+
+    private void bindService(){
+        Intent serviceIntent = new Intent(this, ServiceStressLevel.class);
+        bindService(serviceIntent, mViewModel.getServiceConnection(), Context.BIND_AUTO_CREATE);
+    }
+
+    public void viewHistory(View view){
+        Intent viewHistoryIntent = new Intent(view.getContext(), checkHistory.class);
+        viewHistoryIntent.putExtra("origin", "stressLevel");
+        startActivity(viewHistoryIntent);
+    }
+
 }
